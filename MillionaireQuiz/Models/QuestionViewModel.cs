79edcff3e86699ed1b732d43ec52157b67ec8856
Jarek Blady <jarek.blady97@gmail.com>
2022-11-17 22:46:47 +0100

    add model

---
+++ MillionaireQuiz/Models/QuestionViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MillionaireQuiz.Models
+{
+    public class QuestionViewModel
+    {
+        public int NumberQuestion { get; set; }
+        public string? Question { get; set; }
+        public string? AnswerA { get; set; }
+        public string? AnswerB { get; set; }
+        public string? AnswerC { get; set; }
+        public string? AnswerD { get; set; }
+        public int RewardQuestion { get; set; }
+        public int RewardCurrent { get; set; }
+    }
+}
